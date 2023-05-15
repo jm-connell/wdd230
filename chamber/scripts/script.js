88c1d2480f94af8a361d9cb24072e750e62d426f
jm-connell <con18013@byui.edu>
2023-05-14 22:37:45 -0600

    started date script

--- chamber/scripts/script.js
+++ chamber/scripts/script.js
@@ -1,3 +1,13 @@
 function toggleMenu() {
     document.getElementsByClassName("navigation")[0].classList.toggle("responsive");
 }
+
+const date = new Date();
+
+const fullDate = new Intl.DateTimeFormat("en-UK", {
+	dateStyle: "full"
+}).format(date);
+
+document.querySelector(".headerDate").textContent = `${fullDate}`;
+
+document.querySelector(".footerText").textContent = `© ${date.getFullYear()} Saginaw Chamber | Jon Connell | WDD 230 Project | Last Modification: ${document.lastModified}`;
