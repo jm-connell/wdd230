b1c0886197a589e533ab1fe945c245223360cace
jm-connell <con18013@byui.edu>
2023-05-25 21:12:06 -0600

    added mon/tue banner

--- chamber/scripts/script.js
+++ chamber/scripts/script.js
@@ -1,11 +1,18 @@
 const date = new Date();
 
+
+/* toggle menu with hamburger menu */
 function toggleMenu() {
     document.getElementsByClassName("navigation")[0].classList.toggle("responsive");
 }
 
+/* live date in header and footer */
 const fullDate = new Intl.DateTimeFormat("en-UK", {dateStyle: "full"}).format(date);
-
 document.querySelector('.headerDate').textContent = fullDate;
-
 document.querySelector('.footerText').textContent = `© ${date.getFullYear()} Saginaw Chamber | Jon Connell | WDD 230 Project | Last Modification: ${document.lastModified}`;
+
+/* display meeting banner on mon/tue */
+const day = date.getDay;
+if (day == 1 || day == 2) {
+    document.querySelector(".banner").classList.toggle("meeting");
+}
